43413837d9f886998631d1c3472009008a4097db
msahrana <msahrana@gmail.com>
2023-11-01 23:16:31 +0600

    client site is created

---
+++ src/components/Error/ErrorPage.jsx
@@ -0,0 +1,11 @@
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1 className="text-9xl font-extrabold text-center">404!</h1>
+            <p className="text-center">Page Not Found!!!</p>
+        </div>
+    );
+};
+
+export default ErrorPage;
